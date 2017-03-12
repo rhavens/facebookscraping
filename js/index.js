6c6f207134042ced7a3c6cb896ea7ae67e0e6ec8
Ryan Havens <ryan.d.havens@gmail.com>
2017-03-12 18:36:51 -0400

    New data management strategy for handling refresh across multiple clients

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
 (function(){
+	global_data = {'data': null, 'updated_time': null}
+
 	$(document).ready(function() {
 		console.log('hello, world');
 
@@ -7,26 +9,92 @@
 			console.log(e);
 			if (!refreshPending) {
 				refreshPending = true;
-				$.ajax({
-					url: '/data-refresh',
-					contentType: 'text/plain',
-					dataType: 'json',
-					method: 'GET',
-					success: function(data) { refreshSuccess(data); },
-					error: function(a, b, c) { refreshError(a, b, c); }
-				});
+				makeDataRequest(false);
 			}
 		});
 
+		function makeDataRequest(is_retry) {
+			console.log(is_retry);
+			additional_text = (is_retry ? "?retry=1" : "");
+			return $.ajax({
+				url: '/data-check' + additional_text,
+				contentType: 'text/plain',
+				dataType: 'json',
+				method: 'GET',
+				success: function(data) { refreshSuccess(data); },
+				error: function(a, b, c) { refreshError(a, b, c); }
+			});
+		}
+
 		// handle data successfully loaded
 		function refreshSuccess(data) {
-			refreshPending = false;
-			displayMessage('success', 'Success', 'Successfully fetched data.');
+			// refreshPending = false;
+			console.log(data);
+			if ('data' in data) {
+				// handle the new data
+				global_data['data'] = JSON.parse(data['data']);
+				if ('updated_time' in data) {
+					global_data['updated_time'] = data['updated_time'];
+				}
+				// if this data is stale, initiate a request for new data
+				if ('stale' in data) {
+					if (data['stale'] === '1') {
+						console.log('refreshSuccess stale')
+						$.ajax({
+							url: '/data-refresh',
+							contentType: 'text/plain',
+							dataType: 'json',
+							method: 'GET',
+							success: function(data) { newDataReqSuccess(data); },
+							error: function(a, b, c) { newDataReqFailure(a, b, c); }
+						});
+					}
+					else {
+						refreshPending = false;
+						console.log('refreshSuccess success')
+						displayMessage('success', 'Success', 'Successfully fetched data.');
+						// TODO
+						// notifyUserOfDataUpdate(); // could also just force the data to display at this point
+					}
+				}
+
+			}
+			else {
+				// we are not receiving data because this is a request for data we already possess
+				setTimeout(function() {
+					makeDataRequest(true);
+				}, 15000); // wait 15 seconds before trying again
+			}
+
 		}
 
 		// handle error TODO add retries
 		function refreshError(jqXHR, textStatus, errorThrown) {
 			refreshPending = false;
+			displayMessage('danger', 'Error', 'Something went wrong while fetching the data.');
+		}
+
+		function newDataReqSuccess(data) {
+			// someone else has initiated a request for data already
+			console.log(data);
+			if ('lock' in data) {
+				console.log('newDataReqSuccess lock')
+				setTimeout(function() {
+					makeDataRequest(true);
+				}, 15000); // wait 15 seconds before trying again
+			}
+			else {
+				refreshPending = false;
+				console.log('newDataReqSuccess no lock')
+				displayMessage('success', 'Success', 'Successfully fetched data.');
+				// TODO
+				// notifyUserOfDataUpdate(); // could also just force the data to display at this point
+			}
+		}
+
+		function newDataReqFailure(jqXHR, textStatus, errorThrown) {
+			refreshPending = false;
+			console.log('newDataReqFailure')
 			displayMessage('danger', 'Error', 'Something went wrong while fetching the data.');
 		}
 	});
--- server.py
+++ server.py
@@ -1,3 +1,7 @@
+###############################################################################
+# Dependencies
+###############################################################################
+
 from flask import Flask, request, send_from_directory, make_response
 from flask.ext.pymongo import PyMongo, MongoClient
 from bson.objectid import ObjectId # solve bug: string ID =/= object ID
@@ -11,6 +15,10 @@ import datetime
 import requests
 from dateutil import parser
 
+###############################################################################
+# App initialization
+###############################################################################
+
 app = Flask(__name__, static_url_path='/', static_folder='')
 auth_token = os.getenv('AUTH_TOKEN').strip() # fix bug relating to trailing
 mongo_uri = os.getenv('MONGODB_URI').strip() # whitespace on env variables
@@ -18,7 +26,12 @@ db_name = os.getenv('MONGODB_NAME').strip()
 db = None
 
 # TODO FIX FOR DEBUGGING PURPOSES
-TIME_TO_STALE = 1 # 10 minutes before data becomes stale
+# this is in seconds
+TIME_TO_STALE = 30 # 600 seconds / 10 minutes before data becomes stale
+
+# prevent multiple calls to refresh data - return a helpful notice to clients
+# requesting fresh data while the refresh is locked
+DATA_REFRESH_LOCK = 0
 
 
 # if we are running in a heroku environment, or have a shared db, connect to that
@@ -31,6 +44,9 @@ else:
     with app.app_context():
         db = PyMongo(app).db
 
+###############################################################################
+# Resource access definitions
+###############################################################################
 
 @app.route('/', methods=['GET'])
 def index():
@@ -48,44 +64,100 @@ def send_css(path):
     return send_from_directory('css', path)
 
 
-@app.route('/data-refresh', methods=['GET'])
-def refresh_data():
+###############################################################################
+# Data management definitions
+###############################################################################
+
+# preliminary check to first return data immediately
+# will return an update token if data needs to be updated
+# program is then intended to make a call to data-refresh
+@app.route('/data-check', methods=['GET'])
+def check_data():
+    global DATA_REFRESH_LOCK
+    retry = (request.args.get('retry') or False)
+    print request.args.get('retry')
+    print str(retry)
+
+    # don't send data if this is a retry and we are still fetching data
+    if (retry and DATA_REFRESH_LOCK):
+        return make_response(json.dumps(
+            {
+                'stale': '1'
+            }, default=json_util.default),
+            200, {'ContentType': 'application/json'})
+
     mydata = db.fbdata.find()
     # if we have a recorded previous update time...
     if (mydata.count()):
         # double-check to make sure we have the proper key in the DB response
         if 'updated_time' in mydata[0]:
             # check to see if data is stale
+            # I understand this is convoluted, and I should be using unix time instead, but I'm sick
+            # of reading documentation on timestamps and want to move on
+            # This will cause data-staleness at daylight savings time
+            # Like come on, a 1/3760 failure rate is pretty forgivable
+
+            # data is not stale
             if (datetime.datetime.now() - parser.parse(mydata[0]['updated_time'])).total_seconds() < TIME_TO_STALE:
                 return make_response(json.dumps(
                     {
                         'data': mydata[0]['data'],
-                        'updated_time': mydata[0]['updated_time']
+                        'updated_time': mydata[0]['updated_time'],
+                        'stale': '0'
+                    }, default=json_util.default),
+                    200, {'ContentType': 'application/json'})
+            # data is stale, send a hint to client so it can wait a backoff time and make a new request
+            else:
+                return make_response(json.dumps(
+                    {
+                        'data': mydata[0]['data'],
+                        'updated_time': mydata[0]['updated_time'],
+                        'stale': '1'
                     }, default=json_util.default),
                     200, {'ContentType': 'application/json'})
 
-    # fallback: fetch new data
-    new_data = fetch_data()
-    if (new_data):
-        to_insert = {
-            'updated_time': str(datetime.datetime.now()),
-            'data': json.dumps(new_data, default=json_util.default)
-        }
-        # either insert new data or update old if it exists
-        # try to minimize db size by replacing old data
-        db.fbdata.update({},
-                         {'$set': to_insert},
-                         upsert=True)
-        return make_response(json.dumps(
-            {
-                'data': to_insert['data'],
-                'updated_time': to_insert['updated_time']
-            }, default=json_util.default),
-            200, {'ContentType': 'application/json'})
-    # welp, something went wrong
-    else:
-        return make_response(json.dumps({'error': 'An error occurred fetching data'}), 500, {'ContentType': 'application/json'})
+    # something is wrong - database may be uninitialized or corrupted, requires sysadmin intervention
+    return make_response(json.dumps({'error': 'An error occurred fetching data'}), 500, {'ContentType': 'application/json'})
 
+
+# initiate a request to get new data or notify server of busy state
+@app.route('/data-refresh', methods=['GET'])
+def refresh_data():
+    global DATA_REFRESH_LOCK
+    print str(DATA_REFRESH_LOCK)
+    # avoid refreshing data multiple times, running into race conditions
+    if (DATA_REFRESH_LOCK):
+        return make_response(json.dumps({'lock': '1', 'error': 'Server is busy fetching data'}),
+                             202, {'ContentType': 'application/json'})
+    else:
+        DATA_REFRESH_LOCK = 1
+        new_data = fetch_data()
+        if (new_data):
+            to_insert = {
+                'updated_time': str(datetime.datetime.now()),
+                'data': json.dumps(new_data, default=json_util.default)
+            }
+            # either insert new data or update old if it exists
+            # try to minimize db size by replacing old data
+            db.fbdata.update({},
+                             {'$set': to_insert},
+                             upsert=True)
+            DATA_REFRESH_LOCK = 0
+            return make_response(json.dumps(
+                {
+                    'data': to_insert['data'],
+                    'updated_time': to_insert['updated_time']
+                }, default=json_util.default),
+                200, {'ContentType': 'application/json'})
+        # welp, something went wrong
+        else:
+            DATA_REFRESH_LOCK = 0
+            return make_response(json.dumps({'error': 'An error occurred fetching data'}), 500, {'ContentType': 'application/json'})
+
+
+###############################################################################
+# API access and parsing
+###############################################################################
 
 # fetch data from FB
 # TODO complete the fetch by using the cursors
@@ -119,6 +191,10 @@ def fetch_data():
 
     return data
 
+
+###############################################################################
+# Misc.
+###############################################################################
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
